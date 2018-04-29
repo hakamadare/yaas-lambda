506fbb3d91afadb01971fc2a5cbbc9583de5c485
Steve Huff <shuff@vecna.org>
2018-04-29 15:27:16 +0800

    feat: use AWS event types appropriately

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+  "context"
+  "log"
+
   "github.com/hakamadare/yaas-go/yaas"
+  "github.com/aws/aws-lambda-go/events"
   "github.com/aws/aws-lambda-go/lambda"
 )
 
 func main() {
-  lambda.Start(yaasHandler)
+  lambda.Start(Handler)
 }
 
-func yaasHandler() (string, error) {
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+  log.Print("request ID: %s", request.RequestContext.RequestID)
+
   ys,e := yaas.Yes()
 
-  return string(ys), e
+  if (e == nil) {
+    return events.APIGatewayProxyResponse{Body: string(ys), StatusCode: 200}, e
+  } else {
+    return events.APIGatewayProxyResponse{Body: e.Error(), StatusCode: 500}, e
+  }
 }
